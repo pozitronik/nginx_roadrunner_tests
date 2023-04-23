0bc0d2de1f4a6694c24f26c48d0418646a80f92a
Pozitronik <antikillerxxl@gmail.com>
2023-04-23 13:23:22 +0400

    fixing bootstraps

--- web/index.php
+++ web/index.php
@@ -1,10 +1,7 @@
 <?php
 declare(strict_types = 1);
 
-
 require __DIR__ . '/../vendor/autoload.php';
-
-
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
