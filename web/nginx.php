5db1c703572d15cd301dc9510142d10b85668aaa
Pozitronik <antikillerxxl@gmail.com>
2023-04-23 13:47:44 +0400

    change nginx config to make nginx serve both yii and raw entrypoints

--- web/nginx.php
+++ web/nginx.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types = 1);
 
-$delay = require __DIR__.'/delay.php';
+$delay = require __DIR__.'/../delay.php';
 
 if (true === time_nanosleep(0, $delay??0)) {
 	echo "hello world";
